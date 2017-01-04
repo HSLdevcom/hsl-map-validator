fc663cb40c490b4f6ed70e54b729b0021359543e
Sebastian <sebastian.hognabba@gmail.com>
2017-01-04 09:57:17 +0200

    Split large tile sets into sets of 100 before matching

--- matching/pixelmatch.js
+++ matching/pixelmatch.js
@@ -7,47 +7,68 @@ const pixelmatch = require("pixelmatch");
 const request = require("request");
 const ncp = require('ncp').ncp;
 let tileCounter = 0;
+let tilesTotal = 0;
 
 function getTiles(xyz, options) {
-	const tilesTotal = (xyz.maxX - (xyz.minX - 1)) * (xyz.maxY - (xyz.minY -1));
-	tileCounter = 0;
-	console.log(`${tilesTotal} tiles to compare`)
-
-	for (let x = xyz.minX; x <= xyz.maxX; x++) {
-		for (let y = xyz.minY; y <= xyz.maxY; y++ ) {
-			compareTiles(
-				`${options.url1}/${options.area.zoom}/${x}/${y}.png`, 
-				`${options.url2}/${options.area.zoom}/${x}/${y}.png`,
-				options.zoom, x, y, options.threshold, tilesTotal
-			)
+	return new Promise((resolve) => {
+		let promises = [];
+		for (let x = xyz.minX; x <= xyz.maxX; x++) {
+			for (let y = xyz.minY; y <= xyz.maxY; y++ ) {
+				var promise = compareTiles(
+					`${options.url1}/${options.area.zoom}/${x}/${y}.png`, 
+					`${options.url2}/${options.area.zoom}/${x}/${y}.png`,
+					options.zoom, x, y, options.threshold, tilesTotal
+				);
+				promises.push(promise);
+			}
 		}
-	}
+		Promise.all(promises).then(() => resolve());
+	});
 }
 
 function compareTiles(tile1, tile2, zoom, x, y, threshold, tilesTotal) {
-	let filesRead = 0;
-	let png1 = request.get(tile1).pipe(new PNG());
-	let png2 = request.get(tile2).pipe(new PNG());
-	let img1 = png1.on("parsed", doneReading);
-	let img2 = png2.on("parsed", doneReading);
-	png1.on("error", (error) => console.log(error, `Could not perform match for tile ${tile1}, continuing matching`))
-	png2.on("error", (error) => console.log(error, `Could not perform match for tile ${tile2}, continuing matching`))
+	return new Promise((resolve) => {
+		let filesRead = 0;
+		let png1 = request.get(tile1).pipe(new PNG());
+		let png2 = request.get(tile2).pipe(new PNG());
+		let img1 = png1.on("parsed", doneReading);
+		let img2 = png2.on("parsed", doneReading);
+		png1.on("error", (error) => console.log(error, `Could not perform match for tile ${tile1}, continuing matching`))
+		png2.on("error", (error) => console.log(error, `Could not perform match for tile ${tile2}, continuing matching`))
 
-	function doneReading() {
-		if (++filesRead < 2) return;
+		function doneReading() {
+			if (++filesRead < 2) return;
 		    const diff = new PNG({ width: img1.width, height: img1.height });
 		    const unmatchedPixels = pixelmatch(img1.data, img2.data, diff.data, img1.width, img1.height, {threshold: threshold.colorThreshold});
 		    diff.pack().pipe(fs.createWriteStream(`export/matching/diff/diff-${zoom}-${x}-${y}.png`).on("close", () => {
                 filesRead = 0;
                 tileCounter ++;
                 console.log(`Performed tile match: ${tileCounter} of ${tilesTotal}`);
+                resolve();
 			}));
-	    if (unmatchedPixels > threshold.pixelsCritical) {
-	    	ncp(`export/matching/diff/diff-${zoom}-${x}-${y}.png`, `export/matching/critical/diff-${zoom}-${x}-${y}.png`, function (err) {
-				if (err) return console.error(err);
-			});
-	    }
+		    if (unmatchedPixels > threshold.pixelsCritical) {
+		    	ncp(`export/matching/diff/diff-${zoom}-${x}-${y}.png`, `export/matching/critical/diff-${zoom}-${x}-${y}.png`, function (err) {
+					if (err) return console.error(err);
+				});
+		    }
+		}
+	});
+}
+
+function splitArea(interval, xyz) {
+	let xyzSets = [];
+	for (let x = xyz.minX; x <= xyz.maxX; x = x + interval) {
+		for (let y = xyz.minY; y <= xyz.maxY; y = y + interval) {
+			let xyzSet = {
+				minX: x,
+				minY: y,
+				maxX: Math.min(xyz.maxX, (x + interval - 1)),
+				maxY: Math.min(xyz.maxY, (y + interval - 1)),
+			};
+			xyzSets.push(xyzSet);
+		}
 	}
+	return xyzSets;
 }
 
 module.exports = function(options) {
@@ -64,6 +85,13 @@ module.exports = function(options) {
 		    size: options.area.size,
 		});
 		xyz = merc.xyz(options.area.bbox, options.area.zoom);
-	}	
-	getTiles(xyz, options);
+	}
+	tileCounter = 0;
+	tilesTotal = (xyz.maxX - (xyz.minX - 1)) * (xyz.maxY - (xyz.minY -1));
+	const xyzSets = splitArea(10, xyz);
+	console.log(`${tilesTotal} tiles to compare`)
+	
+	xyzSets.reduce((prev, cur) => {
+		return prev.then(() => getTiles(cur, options))
+	}, new Promise((resolve) => resolve()));
 };
