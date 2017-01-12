71ac076e3c39cb3c96e3d721ffd5985862f74c47
Sebastian <anneliina.hoiva@gmail.com>
2017-01-12 14:27:45 +0200

    Temporarily added digitransit tiles and bbox as defaults, so that match can be run without docker

--- start-match.js
+++ start-match.js
@@ -3,8 +3,8 @@
 const pixelMatcher = require("./matching/pixelmatch");
 
 const options = {
-	url1: process.env.TILE_URL1,
-    url2: process.env.TILE_URL2,
+	url1: process.env.TILE_URL1 || "https://api.digitransit.fi/map/v1/hsl-map",
+    url2: process.env.TILE_URL2 || "https://dev-api.digitransit.fi/map/v1/hsl-map",
     area: {
     	zoom: Number(process.env.ZOOM) || 14,
 	    size:  Number(process.env.SIZE) || 256,
@@ -16,6 +16,6 @@ const options = {
 };
 
 if (process.env.AREA_XYZ) options.area.xyz = JSON.parse(process.env.AREA_XYZ);
-else if (process.env.AREA_BBOX) options.area.bbox = JSON.parse(process.env.AREA_BBOX);
+else options.area.bbox = process.env.AREA_BBOX ? JSON.parse(process.env.AREA_BBOX) : [24.2578125,59.86136748351594,25.551452636718746,60.5153990545698];
 
 pixelMatcher(options);
