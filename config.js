d22aed8dd355ac6fc9022da1aa22b5119f1b93ac
Sebastian <anneliina.hoiva@gmail.com>
2017-01-19 14:14:15 +0200

    Moved default options to config file

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	url1: "https://api.digitransit.fi/map/v1/hsl-map",
+    url2: "https://dev-api.digitransit.fi/map/v1/hsl-map",
+    area: {
+    	bbox: [24.2578125, 59.86136748351594, 25.551452636718746, 60.5153990545698],
+    	zoom: 14,
+	    size: 256,
+    },
+    threshold: {
+    	pixelsCritical: 1000,
+    	colorThreshold: 0.1,
+    },
+};
--- start-match.js
+++ start-match.js
@@ -1,21 +1,27 @@
 "use strict";
 
 const pixelMatcher = require("./matching/pixelmatch");
+const defaultOptions = require("./config.js")
+
+const area = {
+    ...defaultOptions.area,
+    ...process.env.ZOOM && { zoom: Number(process.env.ZOOM) },
+    ...process.env.SIZE && { size: Number(process.env.SIZE) },
+    ...process.env.AREA_XYZ && { xyz: JSON.parse(process.env.AREA_XYZ), bbox: null },
+    ...process.env.AREA_BBOX &&  { bbox: JSON.parse(process.env.AREA_BBOX), xyz: null },
+}
+const threshold = {
+    ...defaultOptions.threshold,
+    ...process.env.PIXELS_CRITICAL && { pixelsCritical: Number(process.env.PIXELS_CRITICAL) },
+    ...process.env.COLOR_THRESHOLD && { colorThreshold: Number(process.env.COLOR_THRESHOLD) }
+}
 
 const options = {
-	url1: process.env.TILE_URL1 || "https://api.digitransit.fi/map/v1/hsl-map",
-    url2: process.env.TILE_URL2 || "https://dev-api.digitransit.fi/map/v1/hsl-map",
-    area: {
-    	zoom: Number(process.env.ZOOM) || 14,
-	    size:  Number(process.env.SIZE) || 256,
-    },
-    threshold: {
-    	pixelsCritical: Number(process.env.PIXELS_CRITICAL) || 1000,
-    	colorThreshold: Number(process.env.COLOR_THRESHOLD) || 0.1,
-    },
+    ...defaultOptions,
+    ...process.env.TILE_URL1 && { url1: process.env.TILE_URL1},
+    ...process.env.TILE_URL2 && { url2: process.env.TILE_URL2},
+    area,
+    threshold,
 };
-
-if (process.env.AREA_XYZ) options.area.xyz = JSON.parse(process.env.AREA_XYZ);
-else options.area.bbox = process.env.AREA_BBOX ? JSON.parse(process.env.AREA_BBOX) : [24.2578125,59.86136748351594,25.551452636718746,60.5153990545698];
 
 pixelMatcher(options);
